4056d28f4f3255302700bf41f6261282a17ac99e
Andrew Eisenberg <aeisenberg@github.com>
2022-06-23 15:36:18 -0700

    Fix

--- .github/check-codescanning-config/index.ts
+++ .github/check-codescanning-config/index.ts
@@ -1,12 +1,22 @@
 
-import core from '@actions/core'
+import * as core from '@actions/core'
 import * as yaml from 'js-yaml'
 import * as fs from 'fs'
 import * as assert from 'assert'
 
 const rawActualConfig = fs.readFileSync(process.argv[2], 'utf8')
+core.startGroup('Actual generated user config')
+core.info(rawActualConfig)
+core.endGroup()
+
 const actualConfig = yaml.load(rawActualConfig)
-const expectedConfig = JSON.parse(core.getInput('expected-config-file-contents'));
+
+const rawExpectedConfig = process.argv[3]
+core.startGroup('Expected generated user config')
+core.info(rawExpectedConfig)
+core.endGroup()
+
+const expectedConfig = rawExpectedConfig ? JSON.parse(rawExpectedConfig) : undefined;
 
 assert.deepStrictEqual(
   actualConfig,
